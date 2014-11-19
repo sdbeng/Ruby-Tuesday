747addb869deb4300761db26b25a7eac0a7d55e6
Sergio Barrera <sdbeng@gmail.com>
2014-11-19 11:16:31 -0800

    Add reopen class in ruby file

---
+++ reopen_class.rb
@@ -0,0 +1,27 @@
+class Tweet
+	attr_accessor :status
+	attr_reader :created_at
+
+	def initialize(status)
+		@status = status
+		@created_at = Time.new
+	end
+
+	def to_s
+		"#{@status}\n#{@created_at}"
+	end
+end
+
+my_tweet = Tweet.new("Playing bingo...")
+puts my_tweet
+
+#using self
+class UserList
+	attr_accessor :name
+	def initialize(name)
+		self.name = name #this calls "name=" on the current object
+	end
+end
+
+list = UserList.new('developers')
+puts "#{list.name}"
